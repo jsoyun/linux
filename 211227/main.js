fbb08c8f5e206f2e281aecd24b8c94a246103c4a
jsoyun <syusmm233@gmail.com>
2021-12-27 16:34:46 +0900

    테스트
    테스트

---
+++ 211227/main.js
@@ -0,0 +1,24 @@
+//변수 선언방식
+
+//var
+
+// var name = 1;
+// console.log(name);
+// var name = 2;
+// console.log(name);
+
+//let
+
+// let name = "bathingape";
+// console.log(name); // bathingape
+
+// let name = "javascript";
+// console.log(name);
+// Uncaught SyntaxError: Identifier 'name' has already been declared
+
+let name = "react";
+console.log(name); //react
+
+name = "1";
+
+console.log(name); //react
