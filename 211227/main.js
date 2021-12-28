c4a4b9454d2e43b59e429c33fa5998b6d09514c8
jsoyun <syusmm233@gmail.com>
2021-12-28 12:49:02 +0900

    변수체크

--- 211227/main.js
+++ 211227/main.js
@@ -16,9 +16,34 @@
 // console.log(name);
 // Uncaught SyntaxError: Identifier 'name' has already been declared
 
-let name = "react";
-console.log(name); //react
+// let name = "react";
+// console.log(name); //react
 
-name = "1";
+// name = "1";
 
-console.log(name); //react
+// console.log(name); //react
+
+// // var kmj = "kkkk";
+// console.log(kmj); // output: undefined
+// var kmj = "kkkk";
+
+// // let ka = "aaaa";
+// console.log(ka); // output: undefined
+// let ka = "aaa";
+// 함수 참조
+console.dir(add); // output: f add(x, y)
+console.dir(sub); // output: undefined
+
+// 함수 호출
+console.log(add(2, 5)); // output: 7
+console.log(sub(2, 5)); // output: Uncaught TypeError: sub is not a function
+
+// 함수 선언문
+function add(x, y) {
+  return x + y;
+}
+
+// 함수 표현식
+var sub = function (x, y) {
+  return x + y;
+};
